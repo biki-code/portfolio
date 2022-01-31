11543dd8b85a9504432c2989cb138c6195234b58
Biki <77373604+biki-code@users.noreply.github.com>
2022-01-31 21:27:52 +0200

    started creating the about page

--- src/components/about/AboutComponent.tsx
+++ src/components/about/AboutComponent.tsx
@@ -7,7 +7,46 @@ const AboutComponent: React.FC<AboutComponentProps> = () => {
   return (
     <>
       <main className={styles.about}>
-        <section>About</section>
+        <div className={styles.flex33}>
+          <section>
+            <h1>Summary</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere cumque voluptatum repellat blanditiis
+              quis quo omnis obcaecati ex delectus id? At laudantium quod vel debitis porro ratione ipsa harum tenetur?
+            </p>
+          </section>
+          <section>
+            <h1>Contact</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere cumque voluptatum repellat blanditiis
+              quis quo omnis obcaecati ex delectus id? At laudantium quod vel debitis porro ratione ipsa harum tenetur?
+            </p>
+          </section>
+          <section>
+            <h1>Skills</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere cumque voluptatum repellat blanditiis
+              quis quo omnis obcaecati ex delectus id? At laudantium quod vel debitis porro ratione ipsa harum tenetur?
+            </p>
+          </section>
+        </div>
+
+        <div className={styles.flex66}>
+          <section>
+            <h1>Work Experience</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere cumque voluptatum repellat blanditiis
+              quis quo omnis obcaecati ex delectus id? At laudantium quod vel debitis porro ratione ipsa harum tenetur?
+            </p>
+          </section>
+          <section>
+            <h1>Education</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere cumque voluptatum repellat blanditiis
+              quis quo omnis obcaecati ex delectus id? At laudantium quod vel debitis porro ratione ipsa harum tenetur?
+            </p>
+          </section>
+        </div>
       </main>
     </>
   );
